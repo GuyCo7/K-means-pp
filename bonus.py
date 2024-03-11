4a302f95beae1a4748ab1e88ed2f947f0ee8de69
Guy Cohen <guyco2204@gmail.com>
2024-03-11 19:53:52 +0200

    feat(plot bounus): running 10 kmeans fro 1-10 clusters and plotting the inertia for each

--- bonus.py
+++ bonus.py
@@ -1,30 +1,41 @@
-import matplotlib.pyplot as plt
 from sklearn import datasets
+from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
+import math
 
 def main():
     
-    
+    iris = datasets.load_iris()
+    vectors = iris.data
+
+    inertias = []    
     for k in range(1, 11):
-        # TODO: run kmeans++
-        # kmeans(k, /.....)
+        kmeans = KMeans(k, init="k-means++", random_state=0).fit(vectors)
         
-        # TODO: compute inertia
-        # inertia(k)
+        centroids = kmeans.cluster_centers_
         
-        pass
+        # Calc the inertia
+        iner = inertia(vectors, centroids)
+        inertias.append(iner)
         
+    # Plot all 10 inertias and save to "elbow.png"
+
+    plt.plot(range(1,11), inertias, marker="o")
+    plt.title("Elbow Method for selection of optimal K clusters")
+    plt.xlabel("K")
+    plt.ylabel("Inertia")
+    plt.annotate(text="elbow point", xy=(2, inertias[1]), xytext=(2+1, inertias[1]+1), arrowprops=dict(facecolor='black', linestyle="--", linewidth=1))
     
-    # TODO: plot all 10 inertias
-    # plt.plot(k, inertia, kind=)
+    plt.savefig("elbow.png")
+    plt.show()
 
     return 0
 
-        
-def inertia(N, vectors, centroids):
+def inertia(vectors, centroids):
     sum = 0
-    for i in range(N):
+    for i in range(len(vectors)):
         current_vector = vectors[i]
-        relevant_centroid = get_centroid_for_vector(vectors[i])
+        relevant_centroid = get_centroid_for_vector(vectors[i], centroids)
         
         sum += difference_squared(current_vector, relevant_centroid)
         
@@ -44,7 +55,18 @@ def difference_squared(vector_x, vector_y):
 
 # returns the relevant centroid for the vector
 def get_centroid_for_vector(vector, centroids):
-    return
+    distances = [euclidean_distance(vector, centroid) for centroid in centroids]
+    min_distance = min(distances)
+    index = distances.index(min_distance)
+    return centroids[index]
+
+# Function to calculate Euclidean distance
+def euclidean_distance(vector_x, vector_y):
+    sum = 0
+    for i in range(len(vector_x)):
+        sum += (float(vector_x[i]) - float(vector_y[i])) ** 2
+    
+    return math.sqrt(sum)
     
 
 if __name__ == "__main__":
